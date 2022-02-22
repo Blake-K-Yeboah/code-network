7e19339831a0d1098d12cf437c535cedf4f149c4
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:31:26 +1000

    Created Text Section Component For Landing Page

---
+++ src/components/LandingPageTextSection/index.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+
+// Prop Types
+import PropTypes from "prop-types";
+
+// Styles
+import styles from "./styles";
+
+// Link Component
+import { Link } from "react-router-dom";
+
+// Animation Imports
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const LandingPageTextSection = ({ text, button, children }) => {
+    const animationVariants = {
+        visible: {
+            opacity: 1,
+            y: 0,
+            scale: 1,
+            transition: { duration: 1, ease: "easeOut" },
+        },
+        hidden: { opacity: 0, y: 150, scale: 0.85 },
+    };
+
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
+
+    return (
+        <motion.section
+            ref={ref}
+            animate={controls}
+            variants={animationVariants}
+            initial="hidden"
+            className={styles.section}
+        >
+            <h3 className={styles.h3}>{text.heading}</h3>
+            <p className={styles.paragraph}>{text.paragraph}</p>
+            {button && (
+                <>
+                    {button.link.external ? (
+                        <>
+                            <a
+                                href={button.link.text}
+                                rel="noreferrer"
+                                target="_blank"
+                            >
+                                <div class={styles.btnContainer}>
+                                    <button className={styles.primaryBtn}>
+                                        {button.content}
+                                    </button>
+                                </div>
+                            </a>
+                        </>
+                    ) : (
+                        <Link to={button.link.text}>
+                            <div class={styles.btnContainer}>
+                                <button className={styles.primaryBtn}>
+                                    {button.content}
+                                </button>
+                            </div>
+                        </Link>
+                    )}
+                </>
+            )}
+            {children}
+        </motion.section>
+    );
+};
+
+// Prop Types
+LandingPageTextSection.propTypes = {
+    text: PropTypes.object,
+    button: PropTypes.object,
+};
+
+export default LandingPageTextSection;
---
+++ src/components/LandingPageTextSection/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+    section: "pt-36 pb-0",
+    h3: "text-5xl font-black text-gray-50 text-center capitalize leading-[1.3em] relative -top-6",
+    paragraph:
+        "text-lg text-slate-400 w-[35%] mx-auto mt-8 text-center leading-loose relative -top-6",
+    primaryBtn:
+        "py-5 px-8 bg-gradient-to-r rounded-lg text-white font-medium transition ease-in-out duration-500 hover:opacity-50 from-blue-400 to-indigo-600",
+    btnContainer: "text-center my-4",
+};
+
+export default styles;
