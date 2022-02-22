39b159f0da0582a9be752eb9b3c3b3d6236839b8
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-23 00:33:38 +1000

    Changed Background Color

--- src/components/LandingPageBoxes/styles.js
+++ src/components/LandingPageBoxes/styles.js
@@ -1,6 +1,6 @@
 const styles = {
-    container: "grid grid-cols-3 gap-8 w-[60vw] h-52 mx-auto my-16",
-    box: "flex flex-col justify-center items-center bg-slate-800 rounded-md relative",
+    container: "grid grid-cols-3 gap-8 w-[60vw] h-56 mx-auto my-16",
+    box: "flex flex-col justify-center items-center bg-gray-800 rounded-md relative",
     iconBox:
         "bg-gradient-to-r from-blue-400 to-indigo-600 text-white flex justify-center items-center w-12 h-12 rounded-md absolute -top-4 shadow-md",
     icon: "text-lg relative -left-[1px]",
