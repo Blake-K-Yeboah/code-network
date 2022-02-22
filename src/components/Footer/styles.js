2b838241ba725666f7bc2500718cbb332f12d58a
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-23 00:33:10 +1000

    Changed Background Color

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,5 +1,5 @@
 const styles = {
-    footer: "bg-slate-900 py-12 text-center mt-24",
+    footer: "bg-gray-900 py-12 text-center mt-24",
     p: "text-md text-slate-500",
     link: "text-indigo-500",
 };
