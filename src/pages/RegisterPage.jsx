8a60ddb755bb6b8bbc9bdfd92e0438645e136e70
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:30:46 +1000

    Created Register Page

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,28 @@
+// useEffect Hook
+import { useEffect } from "react";
+
+// Helmet
+import { Helmet } from "react-helmet";
+
+// Components
+import Navbar from "../components/Navbar";
+import RegisterForm from "../components/RegisterForm";
+
+const Register = () => {
+    useEffect(() => {
+        // Automatic dark mode on register page
+        document.querySelector("body").classList.add("dark", "bg-slate-900");
+    }, []);
+
+    return (
+        <>
+            <Helmet>
+                <title>Code Network - Register</title>
+            </Helmet>
+            <Navbar />
+            <RegisterForm />
+        </>
+    );
+};
+
+export default Register;
