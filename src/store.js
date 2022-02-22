125a8955a3061fbd23e57395b4b139ccaa56b59d
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:25:48 +1000

    Commented code

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+// Reducers
 import authReducer from "./slices/authSlice";
 
+// Export store with all reducers
 export default configureStore({
     reducer: {
         auth: authReducer,
