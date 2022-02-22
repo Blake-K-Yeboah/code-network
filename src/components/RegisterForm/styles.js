43bc2e9a37225a5a114a879929b1559cff32d1d4
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:29:58 +1000

    Created RegisterForm Component

---
+++ src/components/RegisterForm/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+    largeContainer: "h-[92.5vh] w-full flex items-center justify-center",
+    container: "h-[75vh] relative w-[35vw]",
+    title: "text-5xl font-black text-gray-50 text-center capitalize",
+    goBack: "capitalize font-medium text-slate-500 absolute bottom-8 left-16 cursor-pointer transition ease-in-out duration-333 hover:text-blue-500",
+    goBackIcon: "mr-1 text-md inline-block",
+    formGroup: "my-10 w-full relative",
+    formGroupCenter: "mt-12 text-center flex items-center justify-center",
+    label: "block text-gray-50 font-medium text-md mb-3",
+    input: "w-full h-14 px-3 bg-slate-800 focus:bg-slate-600 text-slate-100 focus:outline-none focus:border-b-2 focus:border-blue-500",
+    inputGroup: "flex",
+    inputGroupBox:
+        "w-16 bg-slate-700 text-white flex items-center justify-center text-xl",
+    eye: "cursor-pointer transition ease-in-out duration-333 hover:text-blue-500",
+    primaryBtn:
+        "py-5 px-8 bg-gradient-to-r rounded-lg text-white font-medium from-blue-400 to-indigo-600",
+    secondaryBtn:
+        "py-5 px-8 bg-gradient-to-r rounded-lg font-medium from-slate-500 to-slate-700 text-white",
+};
+
+export default styles;
