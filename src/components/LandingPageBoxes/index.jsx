0a33b12939dd44c5411b61e329060443c9c3f1b1
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:30:16 +1000

    Created LandingPageBoxed Component

---
+++ src/components/LandingPageBoxes/index.jsx
@@ -0,0 +1,53 @@
+// Styles
+import styles from "./styles";
+
+// Prop Types
+import PropTypes from "prop-types";
+
+// Icons
+import { FaShareAlt, FaUsers, FaPlay } from "react-icons/fa";
+
+const Box = ({ details }) => {
+    return (
+        <div className={styles.box}>
+            <div className={styles.iconBox}>{details.icon}</div>
+            <h6 className={styles.title}>{details.title}</h6>
+            <p className={styles.description}>{details.desc}</p>
+        </div>
+    );
+};
+
+// Box Prop Types
+Box.propTypes = {
+    details: PropTypes.object,
+};
+
+const LandingPageBoxes = () => {
+    const boxDetails = [
+        {
+            icon: <FaShareAlt className={styles.icon} />,
+            title: "Share Resources",
+            desc: "Share your favourite resources with the community and browse others",
+        },
+        {
+            icon: <FaUsers className={styles.icon} />,
+            title: "Experience Community",
+            desc: "Be a part of an awesome community of developers just like you who love coding",
+        },
+        {
+            icon: <FaPlay className={styles.icon} />,
+            title: "Play Tech Trivia",
+            desc: "A fun trivia games with various questions about tech so you have fun whilst learning",
+        },
+    ];
+
+    return (
+        <div className={styles.container}>
+            {boxDetails.map((detail) => {
+                return <Box details={detail} key={detail.title} />;
+            })}
+        </div>
+    );
+};
+
+export default LandingPageBoxes;
---
+++ src/components/LandingPageBoxes/styles.js
@@ -0,0 +1,12 @@
+const styles = {
+    container: "grid grid-cols-3 gap-8 w-[60vw] h-52 mx-auto my-16",
+    box: "flex flex-col justify-center items-center bg-slate-800 rounded-md relative",
+    iconBox:
+        "bg-gradient-to-r from-blue-400 to-indigo-600 text-white flex justify-center items-center w-12 h-12 rounded-md absolute -top-4 shadow-md",
+    icon: "text-lg relative -left-[1px]",
+    title: "text-slate-200 font-medium text-lg relative top-4",
+    description:
+        "text-slate-400 text-md w-[62.5%] text-center mx-auto mt-3 relative top-4",
+};
+
+export default styles;
