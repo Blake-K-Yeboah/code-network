e928e003e6bed74e557efc0954fa4403227b7558
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:29:29 +1000

    Created Header Component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,63 @@
+// React Router Dom
+import { Link } from "react-router-dom";
+
+// Icons
+import { FaLevelDownAlt } from "react-icons/fa";
+
+// Styles
+import styles from "./styles.js";
+
+// Framer Motion
+import { motion } from "framer-motion";
+
+const Header = () => {
+    const learnMoreHandler = () => {
+        window.scroll({
+            top: window.innerHeight,
+            left: 0,
+            behavior: "smooth",
+        });
+    };
+    return (
+        <header className={styles.header}>
+            <motion.div
+                animate={{ y: [100, 0], opacity: [0, 1] }}
+                transition={{ ease: "easeOut", duration: 1, delay: 0.5 }}
+            >
+                <h2 className={styles.h2}>
+                    Welcome to a new
+                    <span className={styles.h2Gradient}>digital world</span>
+                </h2>
+                <p className={styles.paragraph}>
+                    Code Network is an open-source social platform that allows
+                    you to meet other developers with similar interests, play
+                    tech trivia and share resources with the awesome tech
+                    community.
+                </p>
+                <div className={styles.div}>
+                    <Link to="/register">
+                        <motion.button
+                            whileHover={{ scale: 1.1 }}
+                            className={`${styles.btn} ${styles.btnPrimary}`}
+                        >
+                            Create an account
+                        </motion.button>
+                    </Link>
+                    <Link to="/login">
+                        <motion.button
+                            whileHover={{ scale: 1.1 }}
+                            className={`${styles.btn} ${styles.btnSecondary}`}
+                        >
+                            Login
+                        </motion.button>
+                    </Link>
+                </div>
+            </motion.div>
+            <span className={styles.learnMore} onClick={learnMoreHandler}>
+                Learn More <FaLevelDownAlt className={styles.learnMoreIcon} />
+            </span>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+    header: "flex justify-center items-center flex-col h-[92.5vh] relative bg-slate-900",
+    h2: "text-6xl font-black text-gray-50 text-center capitalize leading-[1.3em] relative -top-6",
+    h2Gradient:
+        "block bg-gradient-to-r from-blue-400 to-indigo-600 bg-clip-text text-transparent",
+    paragraph:
+        "text-xl text-slate-400 w-[35%] mx-auto mt-8 mb-12 text-center leading-loose relative -top-6",
+    div: "flex items-center justify-center relative -top-6",
+    btn: "py-5 px-8 bg-gradient-to-r rounded-lg text-white font-medium ",
+    btnPrimary: "from-blue-400 to-indigo-600",
+    btnSecondary: "from-slate-500 to-slate-700 ml-8",
+    learnMore:
+        "capitalize font-medium text-slate-500 animate-pulse absolute bottom-8 left-[50%] translate-x-[-50%] cursor-pointer transition ease-in-out duration-333 hover:text-blue-500",
+    learnMoreIcon: "ml-2 text-sm inline-block",
+};
+
+export default styles;
