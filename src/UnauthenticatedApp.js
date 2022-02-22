107470d91f0c4f9f687380749d47b0a15c9e6f48
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-22 22:27:22 +1000

    Added register and login routes

--- src/UnauthenticatedApp.js
+++ src/UnauthenticatedApp.js
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const UnauthenticatedApp = () => {
     return (
         <Routes>
             <Route path="/">
                 <Route index element={<LandingPage />} />
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="login" element={<LoginPage />} />
             </Route>
         </Routes>
     );
