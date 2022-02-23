b5eefa3762533f4bfaf5440313e41065e08ea459
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-23 23:52:33 +1000

    Completed authenticated navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,6 +7,15 @@ import styles from "./styles.js";
 // Framer Motion
 import { motion } from "framer-motion";
 
+// Link Component
+import { Link } from "react-router-dom";
+
+// Icons
+import { FaClone, FaHome, FaQuestionCircle, FaUsers } from "react-icons/fa";
+
+// Components
+import Dropdown from "../Dropdown/index.jsx";
+
 const Navbar = () => {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
@@ -20,22 +29,62 @@ const Navbar = () => {
         </h2>
     );
 
+    const navbarJustifyClass = isAuthenticated
+        ? "justify-between"
+        : "justify-center";
+
+    const getActiveClass = (path) => {
+        if (window.location.pathname === path) {
+            return styles.activeLink;
+        } else {
+            return styles.notActiveLink;
+        }
+    };
+
+    const navLinks = (
+        <div className={styles.navLinksContainer}>
+            <Link to="/" className={`${styles.navLink} ${getActiveClass("/")}`}>
+                <FaHome />
+                Home
+            </Link>
+            <Link
+                to="/users"
+                className={`${styles.navLink} ${getActiveClass("/users")}`}
+            >
+                <FaUsers />
+                Devs
+            </Link>
+            <Link
+                to="/posts"
+                className={`${styles.navLink} ${getActiveClass("/posts")}`}
+            >
+                <FaClone />
+                Posts
+            </Link>
+            <Link
+                to="/trivia"
+                className={`${styles.navLink} ${getActiveClass("/trivia")}`}
+            >
+                <FaQuestionCircle />
+                Trivia
+            </Link>
+        </div>
+    );
+
     return (
-        <>
-            {isAuthenticated ? (
-                <nav className={`${styles.navbar} justify-between`}>
-                    {brand}
-                </nav>
-            ) : (
-                <motion.nav
-                    animate={{ y: [-100, 0], opacity: [0, 1] }}
-                    transition={{ ease: "easeOut", duration: 0.75 }}
-                    className={`${styles.navbar} justify-center`}
-                >
-                    {brand}
-                </motion.nav>
+        <motion.nav
+            animate={{ y: [-100, 0], opacity: [0, 1] }}
+            transition={{ ease: "easeOut", duration: 0.75 }}
+            className={`${styles.navbar} ${navbarJustifyClass}`}
+        >
+            {brand}
+            {isAuthenticated && (
+                <>
+                    {navLinks}
+                    <Dropdown />
+                </>
             )}
-        </>
+        </motion.nav>
     );
 };
 
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,6 +1,11 @@
 const styles = {
     navbar: "w-full flex px-[12.5vw] bg-gradient-to-r from-blue-400 to-indigo-600 h-[7.5vh] items-center relative font-mono",
     brand: "text-white font-bold text-xl tracking-widest lowercase",
+    navLinksContainer: "flex items-center",
+    navLink:
+        "flex items-center justify-center px-3 pt-5 pb-3 flex-col text-md lowercase transition ease-in-out duration-333 cursor-pointer hover:bg-blue-600 hover:text-gray-100",
+    activeLink: "text-white dark:text-gray-900",
+    notActiveLink: "text-gray-300",
 };
 
 export default styles;
