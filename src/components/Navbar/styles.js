e3aa346ca26872d444774e82d1e6c4253b1cc1c3
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-23 23:58:28 +1000

    Fixed Positioning

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,7 +1,7 @@
 const styles = {
     navbar: "w-full flex px-[12.5vw] bg-gradient-to-r from-blue-400 to-indigo-600 h-[7.5vh] items-center relative font-mono",
     brand: "text-white font-bold text-xl tracking-widest lowercase",
-    navLinksContainer: "flex items-center",
+    navLinksContainer: "flex items-center pl-20",
     navLink:
         "flex items-center justify-center px-3 pt-5 pb-3 flex-col text-md lowercase transition ease-in-out duration-333 cursor-pointer hover:bg-blue-600 hover:text-gray-100",
     activeLink: "text-white dark:text-gray-900",
