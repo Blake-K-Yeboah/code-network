c82e5b03a0a689142c976e3d989db5a8ef6d10df
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-23 23:52:01 +1000

    Added Home Page route

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,10 +1,15 @@
 // React Router Stuff
 import { Routes, Route } from "react-router-dom";
 
+// Pages
+import HomePage from "./pages/HomePage";
+
 const AuthenticatedApp = () => {
     return (
         <Routes>
-            <Route path="/" element={<h1>Hello Mr. Authenticated App</h1>} />
+            <Route path="/">
+                <Route index element={<HomePage />} />
+            </Route>
         </Routes>
     );
 };
