4933e409c919aadbdba00b0d10b1a12922dc2653
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-24 22:06:32 +1000

    Added WelcomeModal Component To Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,25 @@
 // Helmet
 import { Helmet } from "react-helmet";
 
+// Redux hooks
+import { useSelector } from "react-redux";
+
 // Components
 import Navbar from "../components/Navbar";
+import WelcomeModal from "../components/WelcomeModal";
 
-const LandingPage = () => {
+const HomePage = () => {
+    const user = useSelector((state) => state.auth.user);
+
     return (
         <>
             <Helmet>
                 <title>Code Network - Home</title>
             </Helmet>
             <Navbar />
+            {user.isNewAccount && <WelcomeModal />}
         </>
     );
 };
 
-export default LandingPage;
+export default HomePage;
