e3649e93981520df1865ed81f881562d438250e6
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-24 22:08:23 +1000

    Fixed positioning of nav links

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,7 +1,7 @@
 const styles = {
     navbar: "w-full flex px-[12.5vw] bg-gradient-to-r from-blue-400 to-indigo-600 h-[7.5vh] items-center relative font-mono",
     brand: "text-white font-bold text-xl tracking-widest lowercase",
-    navLinksContainer: "flex items-center pl-20",
+    navLinksContainer: "flex items-center ml-24",
     navLink:
         "flex items-center justify-center px-3 pt-5 pb-3 flex-col text-md lowercase transition ease-in-out duration-333 cursor-pointer hover:bg-[rgba(255,255,255,.125)] hover:text-gray-100",
     activeLink: "text-white dark:text-gray-900",
