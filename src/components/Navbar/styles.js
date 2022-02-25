9194d81ae95f5dab54b380df1b0eebda06578873
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-25 23:51:04 +1000

    Fixed bug

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,10 +1,10 @@
 const styles = {
     navbar: "w-full flex px-[12.5vw] bg-gradient-to-r from-blue-400 to-indigo-600 h-[7.5vh] items-center relative font-mono",
     brand: "text-white font-bold text-xl tracking-widest lowercase",
-    navLinksContainer: "flex items-center ml-24",
+    navLinksContainer: "flex items-center ml-40",
     navLink:
-        "flex items-center justify-center px-3 pt-5 pb-3 flex-col text-md lowercase transition ease-in-out duration-333 cursor-pointer hover:bg-[rgba(255,255,255,.125)] hover:text-gray-100 hover:border-b-2",
-    activeLink: "text-white dark:text-gray-900 border-b-2 dark:border-gray-900",
+        "flex items-center justify-center px-3 pt-5 pb-3 flex-col text-md lowercase transition ease-in-out duration-333 cursor-pointer hover:bg-[rgba(255,255,255,.125)] hover:text-gray-100 hover:border-t-2",
+    activeLink: "text-white dark:text-gray-900 border-t-2 dark:border-gray-900",
     notActiveLink: "text-gray-300",
 };
 
