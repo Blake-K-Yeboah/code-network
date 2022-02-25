dbee4aba02190dba340262997920b7b3ecdeaf9b
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-25 23:49:12 +1000

    Added usersReducer and postsReducer

--- src/store.js
+++ src/store.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // Reducers
 import authReducer from "./slices/authSlice";
+import usersReducer from "./slices/usersSlice";
+import postsReducer from "./slices/postsSlice";
 
 // Export store with all reducers
 export default configureStore({
     reducer: {
         auth: authReducer,
+        users: usersReducer,
+        posts: postsReducer,
     },
 });
