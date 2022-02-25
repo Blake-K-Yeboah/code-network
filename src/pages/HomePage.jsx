cfbea149144e7b467ae4352dfa7840d35e41e65d
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-25 23:50:34 +1000

    Added Feed Component To Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,42 @@
 import { Helmet } from "react-helmet";
 
 // Redux hooks
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setUsers } from "../slices/usersSlice";
+import { setPosts } from "../slices/postsSlice";
 
 // Components
 import Navbar from "../components/Navbar";
 import WelcomeModal from "../components/WelcomeModal";
+import Feed from "../components/Feed";
+
+// useEffect hook
+import { useEffect } from "react";
+
+// axios
+import axios from "axios";
 
 const HomePage = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // Fetch Users
+        const fetchUsersAndPosts = async () => {
+            try {
+                const usersRes = await axios.get("/api/users-get");
+                dispatch(setUsers(usersRes.data.result));
+
+                const postsRes = await axios.get("/api/posts-get");
+                dispatch(setPosts(postsRes.data.result.reverse()));
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchUsersAndPosts();
+        // eslint-disable-next-line
+    }, []);
+
     const user = useSelector((state) => state.auth.user);
 
     return (
@@ -17,7 +46,7 @@ const HomePage = () => {
                 <title>Code Network - Home</title>
             </Helmet>
             <Navbar />
-            {user.isNewAccount && <WelcomeModal />}
+            {user.isNewAccount ? <WelcomeModal /> : <Feed type="home" />}
         </>
     );
 };
