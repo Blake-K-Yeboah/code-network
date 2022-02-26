ce9fd7a04a255ddac150a929489402b29219092b
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-26 21:32:24 +1000

    Styled dark mode

--- src/components/PostSettings/styles.js
+++ src/components/PostSettings/styles.js
@@ -1,14 +1,14 @@
 const styles = {
     container: "w-full px-8 my-2",
-    heading: "font-medium text-gray-600 mb-2",
+    heading: "font-medium text-gray-600 mb-2 dark:text-gray-300",
     form: "w-full",
     formGroup: "block my-2 w-full",
-    label: "block font-medium text-gray-500 mb-2",
-    input: "w-4/5 bg-gray-100 h-12 p-3 focus:border-b-2 focus:border-blue-500 focus:text-blue-500 focus:outline-none",
+    label: "block font-medium text-gray-500 mb-2 dark:text-gray-400",
+    input: "w-4/5 bg-gray-100 h-12 p-3 focus:border-b-2 focus:border-blue-500 focus:text-blue-500 focus:outline-none dark:bg-gray-700 dark:text-gray-300 dark:focus:text-blue-500",
     formGroupFlex: "flex items-center",
     submitBtn: "text-blue-500 cursor-pointer hover:underline",
     deleteBtn: "text-red-400 cursor-pointer hover:underline",
-    or: "mx-3 my-2",
+    or: "mx-3 my-2 dark:text-gray-300",
 };
 
 export default styles;
