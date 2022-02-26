5acc4b4355b65216ba8aaae3a39fbcdf67b92fcf
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-26 22:51:05 +1000

    Created Profile Component

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,97 @@
+// Styles
+import styles from "./styles";
+
+// Prop Types
+import PropTypes from "prop-types";
+
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+import { updateUser } from "../../slices/usersSlice";
+
+// axios
+import axios from "axios";
+
+// Toast
+import { toast } from "react-toastify";
+
+const Profile = ({ user }) => {
+    const authUser = useSelector((state) => state.auth.user);
+    const token = useSelector((state) => state.auth.token);
+
+    const isFollowing = user
+        ? user.followers.some(
+              (follower) => follower.username === authUser.username
+          )
+        : false;
+
+    const dispatch = useDispatch();
+
+    const followHandler = async () => {
+        try {
+            const res = await axios.put(
+                `/api/users-follow?username=${user.username}`,
+                null,
+                { headers: { authorization: `Bearer ${token}` } }
+            );
+            dispatch(updateUser(res.data));
+            toast.success("Success!", { theme: "colored" });
+        } catch (err) {
+            toast.error("An error occured.", { theme: "colored" });
+        }
+    };
+
+    const followerCount = user ? user.followers.length : "...";
+
+    return (
+        <>
+            {!user ? (
+                <div className={styles.loader} />
+            ) : (
+                <div className={styles.box}>
+                    <div className={styles.header} />
+                    <div className={styles.container}>
+                        <div className="flex">
+                            <img
+                                className={styles.profilePic}
+                                src={`/img/profilePics/${user.profilePic}`}
+                                alt="Profile Pic"
+                            />
+                            <div className={styles.details}>
+                                <h5 className={styles.name}>{user.name}</h5>
+                                <p className={styles.headline}>
+                                    {user.headline || "No Headline"}
+                                </p>
+                            </div>
+                        </div>
+                        {user.username !== authUser.username ? (
+                            <button
+                                className={`${styles.btn} ${
+                                    isFollowing
+                                        ? styles.secondaryBtn
+                                        : styles.primaryBtn
+                                }`}
+                                onClick={followHandler}
+                            >
+                                {isFollowing ? "Following" : "Follow"}{" "}
+                                {followerCount}
+                            </button>
+                        ) : (
+                            <button
+                                disabled
+                                className={`${styles.btn} ${styles.disabledBtn}`}
+                            >
+                                Follow
+                            </button>
+                        )}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+Profile.propTypes = {
+    user: PropTypes.object,
+};
+
+export default Profile;
---
+++ src/components/Profile/styles.js
@@ -0,0 +1,18 @@
+const styles = {
+    box: "w-full h-fit-content bg-white rounded-md dark:bg-gray-800",
+    loader: "w-full h-28 bg-gray-300 dark:bg-gray-700 animate-pulse",
+    header: "bg-gradient-to-r from-blue-400 to-indigo-600 w-full h-20 rounded-t-md",
+    container:
+        "flex flex-row items-center justify-between pl-8 relative -top-6",
+    profilePic:
+        "rounded-full w-24 h-24 border-4 border-white shadow-md dark:border-gray-700",
+    details: "mt-10 ml-4",
+    name: "text-xl font-bold text-gray-600 dark:text-gray-300",
+    headline: "text-md text-gray-400",
+    btn: "py-3 px-5 bg-gradient-to-r rounded-lg text-white font-medium transition-opacity mr-8 mt-8",
+    secondaryBtn: "from-gray-300 to-gray-400 hover:opacity-[.5]",
+    primaryBtn: "from-blue-400 to-indigo-600 hover:opacity-[.5]",
+    disabledBtn: "bg-gray-100 dark:bg-gray-700 text-gray-600",
+};
+
+export default styles;
