ea23ef862fbf5bf4cd1c9f8b2887ed715b5db3d1
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-26 22:50:27 +1000

    Added styling for no posts section

--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -1,6 +1,8 @@
 const styles = {
     container: "w-full pt-6",
     loader: "my-6 w-full h-40 bg-gray-300 dark:bg-gray-700 animate-pulse",
+    noPosts: "w-full p-5 bg-white dark:bg-gray-800",
+    text: "text-md text-gray-600 dark:text-gray-300",
 };
 
 export default styles;
