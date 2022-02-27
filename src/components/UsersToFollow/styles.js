82fbda7d0b4bcaf5aeb3d8e5e62fa3f14ab60e7d
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-27 18:17:54 +1000

    Removed Margin Top

--- src/components/UsersToFollow/styles.js
+++ src/components/UsersToFollow/styles.js
@@ -1,5 +1,5 @@
 const styles = {
-    box: "w-full bg-white drop-shadow-sm dark:bg-gray-800 px-8 pt-6 pb-1 text-gray-300 h-fit-content rounded-md mt-8",
+    box: "w-full bg-white drop-shadow-sm dark:bg-gray-800 px-8 pt-6 pb-1 text-gray-300 h-fit-content rounded-md",
     title: "text-gray-600 font-bold text-xl dark:text-gray-200",
     user: "flex items-center my-6",
     profilePic: "rounded-full w-10 h-10 mr-4",
