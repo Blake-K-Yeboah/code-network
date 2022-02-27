4b4fcaa70d662dcc08edbfd745199289f56309c8
Blake Yeboah <blakekyeboah@gmail.com>
2022-02-27 20:34:50 +1000

    Made Responsive

--- src/components/ModalStep/styles.js
+++ src/components/ModalStep/styles.js
@@ -1,21 +1,23 @@
 const styles = {
     container: "flex flex-col items-center justify-center h-[calc(100%-8px)]",
-    title: "text-4xl font-black text-gray-700 capitalize",
+    title: "text-4xl font-black text-gray-700 capitalize text-center w-[80%]",
     textGradient:
         "bg-gradient-to-r from-blue-400 to-indigo-600 bg-clip-text text-transparent",
     paragraph:
         "text-gray-500 text-md w-[75%] mx-auto mt-8 mb-10 text-center leading-[1.6em]",
     btn: "py-5 px-8 bg-gradient-to-r rounded-lg text-white font-medium ",
     btnPrimary: "from-blue-400 to-indigo-600",
-    btnSecondary: "from-slate-500 to-slate-700 mr-8",
+    btnSecondary: "from-slate-500 to-slate-700 mb-8 sm:mb-0 sm:mr-8",
     input: "w-[75%] mx-auto mb-8 h-14 px-3 bg-gray-200 focus:bg-gray-300 text-gray-900 focus:outline-none focus:border-b-2 focus:border-blue-500 rounded-lg",
-    btnGroup: "flex items-center justify-center",
-    profilePicGrid: "grid grid-cols-4 gap-4 w-[60%] mx-auto mb-8 h-24",
+    btnGroup: "flex flex-col sm:flex-row items-center justify-center",
+    profilePicGrid:
+        "grid grid-cols-2 sm:grid-cols-4 gap-4 w-[90%] md:w-[60%] mx-auto mb-8 h-64 sm:h-24",
     gridBox:
-        "bg-gray-200 flex flex-col items-center justify-center w-full h-full transition duration-333 hover:bg-blue-200 cursor-pointer rounded-lg",
+        "bg-gray-200 flex flex-col items-center justify-center w-full h-full transition duration-333 hover:bg-blue-200 cursor-pointer rounded-lg py-8 sm:py-0",
     activeBox: "bg-gradient-to-r from-blue-400 to-indigo-600",
-    profilePic: "w-[50%] rounded-full",
-    darkModeGrid: "grid grid-cols-2 gap-4 w-[30%] mx-auto mb-8 h-24",
+    profilePic: "min-w-[50px] w-[25%] sm:w-[50%] rounded-full",
+    darkModeGrid:
+        "grid grid-cols-1 sm:grid-cols-2 gap-4 w-[90%] md:w-[30%] mx-auto mb-8 h-72 sm:h-24",
     circle: "w-8 h-8 rounded-full mb-2",
 };
 
--- src/components/WelcomeModal/styles.js
+++ src/components/WelcomeModal/styles.js
@@ -1,7 +1,7 @@
 const styles = {
     backdrop:
         "bg-[rgba(0,0,0,.5)] w-full h-full absolute left-0 top-0 flex items-center justify-center",
-    modal: "w-[40vw] h-[50vh] min-h-[490px] bg-gray-50 rounded-xl relative overflow-hidden",
+    modal: "w-[90vw] lg:w-[60vw] xl:w-[40vw] h-[90vh] sm:h-[50vh] min-h-[490px] bg-gray-50 rounded-xl relative overflow-hidden",
     progressBar:
         "w-[25%] h-4 bg-gradient-to-r from-blue-400 to-indigo-600 rounded-tl-xl",
 };
